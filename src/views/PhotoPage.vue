ef7f965ae3cfad6871eb503e3fa1c962c52473b6
Alexey Alexandrov <alex.androv37@yahoo.com>
2023-09-17 13:20:10 +0300

    Update PhotoPage.vue

--- src/views/PhotoPage.vue
+++ src/views/PhotoPage.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="photo">
-        <h1>{{ $route.params.id }}</h1>
+        <div v-if="photo" class="photo-info">
+            <img :src="photo.user.profile_image.small" :alt="photo.user.name" />
+            <p><strong>User name:</strong> {{ photo.user.name }}</p>
+            <p><strong>User link:</strong> <a :href="photo.user.links.html">{{ userLink  }}</a></p>
+            <button @click="downloadPhoto">Download</button>
+        </div>
         <img :src="photo.urls.regular" :alt="photo.alt_description" />
     </div>
 </template>
 
 <script>
-  import axios from "axios";
+    import axios from "axios";
     export default {
         name: "PhotoPage",
         data() {
             return {
                 photo: null,
+                userLink: "",
             };
         },
         created() {
-            console.log(11111);
             this.fetchPhoto();
         },
         methods: {
@@ -25,10 +30,26 @@
                         `https://api.unsplash.com/photos/${this.$route.params.id}?client_id=Af-22R9C5Ra7DroQiRoqVuroD9nPfSiBd98eW8uLQUs`
                     );
                     this.photo = response.data;
+                    const userLink = response.data.user.links.html;
+                    const userLinkWithoutPrefix = userLink.replace("https://unsplash.com/", "");
+                    this.userLink = userLinkWithoutPrefix;
                 } catch (error) {
                     console.error(error);
                 }
-            }
+            },
+            downloadPhoto() {
+// Create a link element with the photo download url
+const link = document.createElement("a");
+link.href = this.photo.links.download;
+// link.href = window.URL.createObjectURL(new Blob([response.data]));
+link.download = true;
+// Append the link to the document body and click it
+// link.setAttribute('download', 'image.jpg')
+document.body.appendChild(link);
+link.click();
+// Remove the link from the document body
+document.body.removeChild(link);
+},
         }
     }
 </script>
